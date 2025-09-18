82aacdb3e1d47d67b14921e2535ca39870bb545f
olihudson1992 <oliver.david.hudson@gmail.com>
2025-09-18 23:14:29 +0100

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,16 @@
+Origin and history of weird
+weird(adj.)
+c. 1400, "having power to control fate," in weird sisters, from weird (n.) "force that sets events in motion or determines their course; what is destined to befall one;" from Old English wyrd "fate, chance, fortune; destiny; the Fates." The modern senses developed from weird sisters, not immediately from the Old English word, which is etymologically "that which comes."
+
+It is reconstructed to be from Proto-Germanic *wurthiz (source also of Old Saxon wurd, Old High German wurt "fate," Old Norse urðr "fate, one of the three Norns"), itself reconstructed to be from PIE *wert- "to turn, to wind" (source also of German werden, Old English weorðan "to become"), from root *wer- (2) "to turn, bend." For the sense development from "turning" to "becoming," compare colloquial phrase turn into "become." 
+
+The sense of "uncanny, supernatural" developed from Middle English use of weird sisters for the three Fates, Parcae, or Norns (in Germanic mythology), the goddesses who controlled human destiny. They were portrayed as odd or frightening in appearance, as in "Macbeth" (especially in 18th and 19th century productions).
+
+The modern adjectival use, without sisters, emerged early 19c. Todd's supplement to Johnson (1818) has it as "skilled in witchcraft." Shelley was perhaps the first to use it consistently in print as "supernatural, uncanny":
+
+                In lone and silent hours,
+When night makes a weird sound of its own stillness,
+["Alastor"]
+The weakened meaning "odd-looking, strange, disturbingly different" followed (1820). Also see Macbeth. Related: Weirdly; weirdish; weirdness.
+
+As a verb, "change by witchcraft or sorcery." Earlier to be weirded in Middle English was "be foreordained or predestined."
