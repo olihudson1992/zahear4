0d4ce4ad19dc73043613b8f3c2dc3492f22f753a
olihudson1992 <oliver.david.hudson@gmail.com>
2026-04-29 14:50:29 +0100

    Change favicon emoji from aquarius to star

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css"
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "♒",
+  title: "⭐",
   description: "WYRD",
   generator: "v0.dev",
   icons: {
